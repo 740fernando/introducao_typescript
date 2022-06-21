ffff89c9ebc07de10ba2ff9e73c253f3a195aa01
Fernando <fernando.luiz92@hotmail.com>
2022-06-21 07:34:05 -0300

    Tratando a tag input

--- 02 - Prática/index.ts
+++ 02 - Prática/index.ts
@@ -1,46 +1,6 @@
-function soma(a: number , b: number){
-    return a+b;
-}
+const input = document.getElementById('input') as HTMLInputElement;
 
-// types
-// interfaces
-
-interface IAnimal{
-    nome: string;
-    tipo: 'terrestre' | 'aquático';
-    rugir(alturaEmDecibeis: number): void;
-    domestico: boolean
-}
-
-interface IFelino extends IAnimal{
-    visaoNoturna: boolean;
-}
-
-const animal: IAnimal = {
-    nome: 'Elefante',
-    tipo: 'terrestre',
-    rugir: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
-    domestico: true
-}
-
-const felino: IFelino = {
-    nome: 'Leão',
-    tipo: 'terrestre',
-    visaoNoturna: true,
-    rugir: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
-    domestico: true
-}
-
-interface ICanino extends IAnimal{
-     porte: 'pequeno' | 'medio' | 'grande';
-}
-
-type IDomestico = IFelino | ICanino; // tipos são usados para fazer juncoes
-
-const animalMultiplasInterfaces : IDomestico = {
-    domestico: true,
-    nome: 'cachorro',
-    porte: 'medio',
-    tipo: 'terrestre',
-    rugir: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
-}
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+});
